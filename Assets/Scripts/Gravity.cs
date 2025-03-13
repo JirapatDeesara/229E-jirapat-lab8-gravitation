5d8dc653b8d743528c1a95637f1146d58da1ae06
JirapatDeesara <jirapat.dees@bumail.net>
2025-03-13 10:05:15 +0700

    Attract Scripts

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    Rigidbody rb;
+    const float G = 0.006674f; // Gravitational constant 6.674 force in Universe
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-}
+    void Attract(Gravity other)
+    { 
+     Rigidbody otherRb = other.rb;
+     
+        // Distance = PositionA - PositionB
+        Vector3  direction = rb.position - other.rb.position;
+        float distance = direction.magnitude; // magnitude use to tell only distance
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized; // normalize use to tell direction
+        // addforce with direction!
+        otherRb.AddForce(finalForce);
+    }// End Method Attract
+}// End Gravity
