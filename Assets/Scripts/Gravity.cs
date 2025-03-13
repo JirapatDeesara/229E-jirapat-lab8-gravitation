16041ae3182a43c2df220eb8c4f51b41decd3314
JirapatDeesara <jirapat.dees@bumail.net>
2025-03-13 10:45:33 +0700

    Add Orbit speed
    -Add orbit

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,14 +1,44 @@
+using NUnit.Framework;
+using System.Collections.Generic; // for List
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
     Rigidbody rb;
     const float G = 0.006674f; // Gravitational constant 6.674 force in Universe
+    // Array for containing all the items
+    public static List<Gravity> gravityObjectList;
 
+    // Set speed for orbiting the planet
+    [SerializeField] bool planet = false; // if it's not a planet = orbit
+    [SerializeField] int oribitSpeed = 1000;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-    }
+        if (gravityObjectList == null) { 
+        gravityObjectList = new List<Gravity>();
+        }// if not created then create first
+        gravityObjectList.Add(this);
+
+        // orbiting
+        if (!planet)
+        {
+            rb.AddForce(Vector3.left * oribitSpeed);
+        }// End if not a planet
+    }//End Awake
+
+    private void FixedUpdate()
+    {
+        // Use Planet in each Array that contatin gravity attract each other
+        foreach (var obj in gravityObjectList)
+        { 
+         if (obj != this) 
+            {
+                Attract(obj);
+            }// end if for Attract other not yourself!
+        }
+    }// End FixedUpdate
+
     void Attract(Gravity other)
     { 
      Rigidbody otherRb = other.rb;
